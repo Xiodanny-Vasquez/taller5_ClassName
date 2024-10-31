e28dbeab1840b02cb29fc0771a070fd3f88f7342
Xiodanny-Vasquez <xiodanny.vasquez@soy.sena.edu.co>
2024-10-30 23:48:02 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ProfileCard from "./components/ProfileCard";
+const App = () => {
+  return (
+      <div className="tw-flex tw-flex-col tw-ustify-center tw-items-center  tw-min-h-screen">
+        <ProfileCard/>
+      </div>
+  );
+};
+
+export default App;
---
+++ src/components/ProfileCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./ProfileCard.css"; 
+
+const ProfileCard = () => {
+  return (
+    <div className="profile-card">
+      <h2 className="profile-name">XIODANNY VASQUEZ HOLGUIN</h2>
+      <p className="profile-description">
+        ♥ Desarrolladora Full Stack, Creativa e Innovadora ♥ 
+      </p>
+    </div>
+  );
+};
+
+export default ProfileCard;
